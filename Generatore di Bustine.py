b49ba5d405bf71305bc057a23991b630eedd94c7
zgruntolo <103127204+zgruntolo@users.noreply.github.com>
2024-02-16 09:23:37 +0100

    Add files via upload

--- Generatore di Bustine.py
+++ Generatore di Bustine.py
@@ -1,23 +1,48 @@
+import csv
 import random
+from enum import Enum
 
-def creazione_bustina(soglie, carte):
-    rarita_scelta = random.choices(list(carte.keys()), weights=soglie)[0]
-    carta_scelta = random.choice(carte[rarita_scelta])
-    return carta_scelta
+class Rarity(Enum):
+    COMMON = "Common"
+    UNCOMMON = "Uncommon"
+    RARE = "Rare"
 
-def aprire_bustina():
-    carte = {
-        "comune": ['Melm Caramel', 'Melm Magimel', 'Melm Shieldmel', 'Pirocinghiale', 'Grenado', 'Insetto Fuliggine', 'Explocactus', 'Cacknight', 'Cacbard', 'Cacpriest' ,'Belladonna Timida', 'Glicine Labirinto (Viticcio Violento)', 'Glicine Labirinto (Viticcio Spinoso)', 'Glicine Labirinto (Viticcio Radice)', 'Glicine Labirinto (Viticcio Protettivo)', 'Glicine Labirinto (Viticcio Magico)', 'Servobot Ripristinatore', 'Servobot Manipolatore', 'Servobot Assistente', 'Golem da Combattimento', 'Pescespina (pesce)', 'Pinnaculeo (pesce)', 'Pinnaiuto (pesce)', 'Pinnascudo (pesce)', 'King Crab (Chela di Protezione)', 'King Crab (Chela di Attacco)', 'King Crab (Chela Magica)', 'Spargitore Micoide', 'Micoide', 'Micoidemante (Micoide)', 'Servobot Caricatore R', 'Servobot Caricatore B', 'Servobot Caricatore V', 'King Melm (mano del comando)', 'King Melm (mano della spada)', 'King Melm (mano dello scudo)', 'Bambola da c. (Arto canalizzatore)', 'Bambola da c. (Arto protettivo)', 'Bambola da c. (Arto multiplo)', 'Bambola da c. (Arto estrapolante)', 'Bambola da c. (testa sacrificale)', 'Bambola da c. (testa subacquea)', 'Bambola da c. (testa batteria)', 'Bambola da c. (magicannone)', 'Bambola da c. (magimortaio)', 'Demidrago (Testa di Fuoco)', 'Demidrago (Testa di Fulmine)', 'Demidrago (Testa di Terra)', 'Demidrago (Testa di Aria)', 'Idrozoa', 'Terrordottero', 'Draco Rosso', 'Draco Verde', 'Draco Blu'],
-        "non_comune": ['Melm Knightmel', 'Pirocinghiale Capobranco', 'Rovo Senziente', 'Megaservobot Ripristinatore', 'Micoide Alfa', 'Micoidemante Alfa (Micoide)', 'Melm Acemel', 'King Melm', 'Agnello Vegetale', 'Megaservobot Manipolatore', 'Servobot Sabotatore', 'Golem Amplificatore', 'Medusa', 'King Crab', 'Pescebanco (pesce)', 'Bambola da combattimento', 'Aviano delle Bambole', 'Bambola cingolata', 'Bambola da c. (magilaser)', 'Demidrago', 'Terrordottero Parassita', 'Malocchio rosso', 'Malocchio azzurro', 'Malocchio verde', 'Coccatrice', 'Malocchio vitreo', 'Terrordottero cacciacarcasse', 'Demone delle Maree', 'Demone Spinato'],
-        "rara": ['Pirocinghiale Mistico', 'Dinocorno (Unicorno)', 'Fogliacorno (Unicorno)', 'Glicine Labirinto', 'Robocorno (Unicorno)', 'Narvacorno (Unicorno)', 'Spinalodonte (pesce)', 'Pegacorno (Unicorno)', 'Spargitore Micoide Supremo', 'Acchiappadraghi', 'Golem Fortezza', 'Tigre Bianca', 'Demidrago Meccanico', 'Arcanum del Cancello', 'Arcanum del Cielo', 'Arcanum del Forgia', 'Arcanum del Gelo', 'Demone del Baratto', 'Demone del Sacrificio', 'Demone delle Ombre']
-    }
+class Card:
+    def __init__(self, name, rarity):
+        self.name = name
+        self.rarity = rarity
 
-    soglie = [(3, 2, 1), (3, 2, 1), (2, 3, 1), (1, 3, 2)]
+def generate_rarity_weights():
+    return [
+        [3, 2, 1],
+        [3, 2, 1],
+        [2, 3, 1],
+        [2, 2, 2]
+    ]
 
-    bustina = [creazione_bustina(soglie, carte) for soglie in soglie]
+def create_card_pool(filename):
+    card_pool = {Rarity.COMMON: [], Rarity.UNCOMMON: [], Rarity.RARE: []}
+    with open(filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            rarity = Rarity[row['rarity']]
+            card = Card(row['name'], rarity)
+            card_pool[rarity].append(card)
+    return card_pool
 
-    for carta in bustina:
-        print(f"{carta}")
+def create_pack(cards, rarity_weights):
+    chosen_rarity = random.choices(list(Rarity), weights=rarity_weights)[0]
+    chosen_card = random.choice(cards[chosen_rarity])
+    return chosen_card
+
+def open_pack(cards):
+    
+    rarity_weights = generate_rarity_weights()
+    pack = [create_pack(cards, rarity_weights) for rarity_weights in rarity_weights]
+    for card in pack:
+        print(f"{card.name} - Rarity: {card.rarity.value}")
 
 if __name__ == "__main__":
-    aprire_bustina()
+    card_pool = create_card_pool("cards.csv")
+    
+    open_pack(card_pool)
